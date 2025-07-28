8e72c3218ea8144dd2b25039c88fad575f3c57a9
Arif Ansari <arifkmclu@gmail.com>
2025-07-28 18:23:49 +0530

    Login

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcrypt from 'bcryptjs'
+import jwt from "jsonwebtoken"
+
 
 export const register = async (req, res) => {
     try {
@@ -8,7 +10,8 @@ export const register = async (req, res) => {
         if (!fullName || !email || !phoneNumber || !password || !role) {
             return res.status(400).json({
                 message: 'Something is missing',
-                success: false
+                success: false,
+                
             });
         }
 
@@ -42,4 +45,50 @@ export const register = async (req, res) => {
             success: false
         });
     }
+}
+
+export const login = async(req, res) => {
+ try{
+    const {email, password, role} = req.body;
+    if (!email || !password || !role) {
+        return res.status(400).json({
+            message: 'Something is missing',
+            success: false,
+        });
+    };
+    const user = await User.findOne({email});
+    if(!user){
+        return res.status(400).json({
+            message:"Incorrect Email or password",
+            success:false
+        })
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if(!isPasswordMatch){
+        return res.status(400).json({
+            message:'Incorrect Email or password',
+            success:false,
+        })
+    }
+    //check role is correct or not
+    if(role !== user.role){
+        return res.status(400).json({
+            message:"Account doesn't exist with current role",
+            success:false
+        })
+    };
+    const tokenData = {
+        userId:user._id
+    }
+    const token = await jwt.sign(tokenData,process.env.SECRET_KEY,)
+ }
+
+ catch(error){
+    console.error("Error during Login:", error);
+        return res.status(500).json({
+            message: 'Internal server error',
+            success: false
+        });
+
+ }
 }
